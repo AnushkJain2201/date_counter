b26d57d8beef17dd2b1e7b50ba4279e3e41784b7
Anushk Jain <anushk2201@gmail.com>
2023-12-09 08:18:14 +0530

    Basic Structure Done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,21 @@ export default App;
 const Counter = () => {
   return (
     <div>
-      Test
+      <div className='ter'>
+        <button className='btn'>-</button>
+        <h3>Step: 0</h3>
+        <button className='btn'>+</button>
+      </div>
+
+      <div className='ter'>
+        <button className='btn'>-</button>
+        <h3>Count: 0</h3>
+        <button className='btn'>+</button>
+      </div>
+
+      <div className='ter'>
+        <h3>Sat Dec 09 2023</h3>
+      </div>
     </div>
   );
 }
